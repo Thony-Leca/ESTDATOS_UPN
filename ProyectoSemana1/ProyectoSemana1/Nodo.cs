a1da0666f11d102866e462cf3a38a334711636b7
Thony Leca <antholeca45@gmail.com>
2026-03-28 21:18:50 -0500

    Semana 1

---
+++ ProyectoSemana1/ProyectoSemana1/Nodo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoSemana1
+{
+    public class Nodo
+    {
+        // campos o atributos
+        public int info { get; set; }
+        public Nodo sig { get; set; }
+    }
+}
