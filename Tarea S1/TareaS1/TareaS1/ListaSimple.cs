9be93e340b9fa7d3c856a5ebab03eddc0a56256e
Thony Leca <antholeca45@gmail.com>
2026-04-03 10:32:11 -0500

    feat: nuevos archivos de la actividad de practica

---
+++ Tarea S1/TareaS1/TareaS1/ListaSimple.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TareaS1
+{
+    public class ListaSimple
+    {
+        Nodo raiz, nuevo, aux, ptr; // ptr = pointer = puntero = direccion direccion = numero
+
+        // Método Constructor
+        public ListaSimple() { raiz = null; }
+
+        // Método InsertarLIFO
+        public void InsertarLIFO(string dato)
+        {
+            nuevo = new Nodo();
+            nuevo.info = dato;
+            if (raiz == null)
+            {
+                nuevo.sig = null; raiz = nuevo;
+            }
+            else
+            {
+                nuevo.sig = raiz; raiz = nuevo;
+            }
+        }
+        // Método InsertarFIFO
+        public void InsertarFIFO(string dato)
+        {
+            nuevo = new Nodo();
+            nuevo.info = dato;
+            if (raiz == null)
+            {
+                nuevo.sig = null; raiz = aux = nuevo;
+            }
+            else
+            {
+                aux.sig = nuevo; nuevo.sig = null; aux = nuevo;
+            }
+        }
+
+        // Método Recorrido
+        public void Recorrido()
+        {
+            ptr = raiz;
+            while (ptr != null)
+            {
+                Console.Write("{0},", ptr.info);
+                ptr = ptr.sig;
+            }
+        }
+
+        // Método Buscar
+        public int Buscar(string dato)
+        {
+            int resultado = 0;
+            ptr = raiz;
+            while (ptr != null)
+            {
+                if (ptr.info.ToLower() == dato.ToLower())
+                {
+                    resultado = 1; break;
+                }
+                ptr = ptr.sig;
+            }
+            return resultado;
+        }
+
+        // Método Eliminar
+        public void Eliminar(string dato)
+        {
+            bool resultado = false;
+            if (raiz.info.ToLower() == dato.ToLower())
+            {
+                resultado = true; raiz = raiz.sig;
+            }
+            else
+            {
+                ptr = aux = raiz;
+                while (ptr != null)
+                {
+                    if (ptr.info.ToLower() == dato.ToLower())
+                    {
+                        resultado = true; aux.sig = ptr.sig;
+                        break;
+                    }
+                    aux = ptr; ptr = ptr.sig;
+                }
+            }
+            Console.WriteLine("{0} ELIMINADO", resultado ? "SI" : "NO");
+            Recorrido();
+        }
+
+        // Método Ordenar
+        public void Ordenar()
+        {   // > (Ascendente), < (Descendente)
+            ptr = raiz;
+            while (ptr != null)
+            {
+                aux = raiz;
+                while (aux != null)
+                {
+                    // Comparamos si los valores string son mayores o menores por orden alfabético
+                    if (string.Compare(ptr.info, aux.info) > 0)
+                    {
+                        string temp = ptr.info;
+                        ptr.info = aux.info;
+                        aux.info = temp;
+                    }
+                    aux = aux.sig;
+                }
+                ptr = ptr.sig;
+            }
+        }
+    }
+}
+
---
+++ Tarea S1/TareaS1/TareaS1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TareaS1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ListaSimple lista = new ListaSimple();
+            // Añadir 5 elementos
+            lista.InsertarFIFO("Cesar");
+            lista.InsertarFIFO("Anthony");
+            lista.InsertarFIFO("José");
+            lista.InsertarFIFO("Joshimar");
+            lista.InsertarFIFO("Renato");
+            // Listar los elementos
+            Console.WriteLine("--- LISTADO DE ELEMENTOS ---");
+            lista.Recorrido();
+            // Ordenar los elementos
+            lista.Ordenar();
+            // Listar los elementos
+            Console.WriteLine("--- LISTADO DE ELEMENTOS ---");
+            lista.Recorrido();
+            // Buscar elemento
+            string dato;
+            do
+            {
+                Console.Write("\nDATO A BUSCAR ('fin' FIN):");
+                dato = Console.ReadLine();
+                Console.WriteLine("{0} {1} ENCONTRADO", dato,
+                    lista.Buscar(dato) == 1 ? "SI" : "NO");
+            } while (dato != "fin");
+            // Eliminar elemento
+            do
+            {
+                Console.Write("\nDATO A ELIMINAR ('fin' FIN):");
+                dato = Console.ReadLine();
+                lista.Eliminar(dato);
+            } while (dato != "fin");
+
+            Console.ReadLine();
+        }
+    }
+}
